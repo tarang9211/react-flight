7163d23fdd8e138ca07e41ed62cb0da251bd3c73
Tarang Hirani <tarang9211@gmail.com>
2017-03-04 00:26:29 +0530

    updated dependencies, set up basic scafolding for the site

---
+++ src/components/flight_listing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FlightListing extends Component {
+  render() {
+    return(
+      <div className="flight-listing-wrapper">
+        Listing
+      </div>
+    );
+  }
+}
+
+export default FlightListing
